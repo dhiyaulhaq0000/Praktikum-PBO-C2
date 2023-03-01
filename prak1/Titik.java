f1a174b06c64981c7219250d2a54e4bdb8b015b5
Dhiya Ulhaq <100354518+dhiyaulhaq0000@users.noreply.github.com>
2023-03-01 20:15:20 +0700

    Add files via upload

---
+++ prak1/Titik.java
@@ -0,0 +1,31 @@
+class Titik{
+	double absis;
+	double ordinat;
+	static int counterTitik;
+	
+	Titik(){
+		absis = 0;
+		ordinat = 0;
+		counterTitik++;
+		}	
+		Titik(double a,double o){
+			counterTitik++;
+			absis = a;
+			ordinat = o;
+	}
+	void setAbsis(double a){
+		absis=a;
+	}
+	void setOrdinat (double o) {
+		ordinat = o;
+	}
+	double getAbsis(){
+		return absis;
+	}
+	double getOrdinat(){
+		return ordinat;
+	}
+	int getCounterTitik(){
+		return counterTitik;
+	}
+}
