5b103dacd48260fa7c23d8cac38e8c1025fdd10b
Dhiya Ulhaq <100354518+dhiyaulhaq0000@users.noreply.github.com>
2023-06-06 15:06:32 +0700

    Add files via upload

---
+++ prak9/MainDAO.java
@@ -0,0 +1,13 @@
+public class MainDAO {
+    public static void main(String[] args) {
+        Person person = new Person("Indra");
+        DAOManager daoManager = new DAOManager();
+        daoManager.setPersonDAO(new MySQLPersonDAO());
+        
+        try {
+            daoManager.getPersonDAO().savePerson(person);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
